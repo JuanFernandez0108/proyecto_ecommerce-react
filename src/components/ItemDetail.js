b9b03581485ec032b412541615f7fdba517bf6bc
Juan Fernandez <juanfernandez0108@gmail.com>
2022-11-10 19:00:24 -0300

    Segunda entrega del proyecto final

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,12 +2,12 @@ const ItemDetail = (props) => {
     return(
         <div className="item_detail">
             <div className="image_box_detail">
-                <img className="image_detail" src={props.imagenDetail} alt="" />
+                <img className="image_detail" src={props.item.imagenDetail} alt="" />
             </div>
             <div className="details_detail">
-                <h5>{props.titulo}</h5>
-                <p>{props.descripcion}</p>
-                <span className="simbol_price_detail"></span><span className="price_span_detail">{props.precio}</span>
+                <h5>{props.item.titulo}</h5>
+                <p>{props.item.descripcion}</p>
+                <span className="price_span_detail">${props.item.precio}</span>
                 <button className="btn_detail">Agregar al carrito</button>
             </div>
         </div>
